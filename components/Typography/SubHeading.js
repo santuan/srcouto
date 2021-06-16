8e02e08bba039f29c04616ec783a0e78291f1e1f
santuan <smrcouto88@gmail.com>
2021-06-16 02:40:37 -0300

    accesibility

--- components/Typography/SubHeading.js
+++ components/Typography/SubHeading.js
@@ -1,11 +1,11 @@
 const SubHeading = ({ title }) => {
   return (
     <div className="w-full ">
-      <h3 className="w-full px-4 pb-4 mt-20 mb-12 font-sans text-base font-bold text-black border-b-2 border-gray-50 md:text-2xl dark:text-white dark:border-gray-800">
+      <h2 className="w-full px-4 pb-4 mt-20 mb-12 font-sans text-base font-bold text-black border-b-2 border-gray-50 md:text-2xl dark:text-white dark:border-gray-800">
         <span className="pb-4 border-b-4 border-gray-600 dark:border-white">
           {title}
         </span>
-      </h3>
+      </h2>
     </div>
   );
 };
