fe1ace8ae1bf1b8b9fa2688fcb2b2723cc00e8a2
santuan <smrcouto88@gmail.com>
2021-06-22 00:39:04 -0300

    webmanifest

--- components/Bio/Timeline.js
+++ components/Bio/Timeline.js
@@ -16,19 +16,64 @@ const Year = ({ children }) => {
   );
 };
 
-const Step = ({ title, link, children }) => {
+const Techs = ({ children }) => {
   return (
-    <li className="relative mx-4 mb-12 md:mb-10">
-      <div className="flex items-start mb-2 text-gray-700 dark:text-gray-300">
-        <FiChevronsRight className="relative top-1.5" />
+    <ul className="flex mt-3 space-x-3 font-sans text-base font-bold text-gray-900 dark:text-white">
+      {children}
+    </ul>
+  );
+};
+
+const TechsItem = ({ title }) => {
+  return <li>{title}</li>;
+};
+
+const ProjectsLinks = ({ portfolioLink, webLink }) => {
+  return (
+    <div className="absolute right-0 flex justify-start space-x-6 border-gray-400 left-8 md:left-auto md:space-x-3 bottom-2 md:bottom-12 borber-b ">
+      {portfolioLink && (
         <a
-          href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="ml-4 text-xl font-medium text-gray-900 uppercase dark:text-gray-100 hover:text-blue-500 dark:hover:text-blue-500"
+          className="pb-1 text-base font-bold text-gray-800 border-b-2 border-blue-500 md:text-sm hover:text-blue-500 dark:hover:text-blue-500 dark:border-blue-500 dark:text-white"
+          href={portfolioLink}
+        >
+          Ver portfolio
+        </a>
+      )}
+      {webLink && (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="pb-1 text-base font-bold text-gray-800 border-b-2 border-blue-500 md:text-sm hover:text-blue-500 dark:hover:text-blue-500 dark:border-blue-500 dark:text-white"
+          href={webLink}
+        >
+          Visitar web
+        </a>
+      )}
+    </div>
+  );
+};
+
+const BigStep = ({ children }) => {
+  return (
+    <ul className="w-full">
+      {children}
+    </ul>
+  );
+};
+
+const Step = ({ title,  children }) => {
+  return (
+    <li className="relative w-full pt-12 pb-16 mx-4 border-t border-gray-300 md:py-12 dark:border-gray-700">
+      <div className="flex items-start mb-2 text-gray-700 dark:text-gray-300">
+        <FiChevronsRight className="relative top-1.5" />
+        <div
+          rel="noopener noreferrer"
+          className="ml-4 text-xl font-medium text-gray-900 uppercase dark:text-gray-100"
         >
           {title}
-        </a>
+        </div>
       </div>
       <p className="ml-8 text-gray-700 dark:text-gray-400">{children}</p>
     </li>
@@ -51,112 +96,88 @@ export default function Timeline() {
     <>
       <SubHeading title="Páginas web realizadas" />
       <Year>2021</Year>
-      <ul>
+      <BigStep>
         <Step title="Santuan" link="https://www.santuan.com.ar/">
           Versión experimental utilizando el mismo contenido de este sitio pero
           con un impronta más visual.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Gatsby.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Contentful
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Netlify
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Gatsby.js" />
+            <TechsItem title="Contentful" />
+            <TechsItem title="Netlify" />
+          </Techs>
         </Step>
-      </ul>
-      <ul>
+      </BigStep>
+      <BigStep>
         <Step
           title="Bomberos Voluntarios Luján de Cuyo"
           link="https://bomberoslujandecuyo.org.ar/"
         >
           El Cuerpo de Bomberos Voluntarios de Luján fue fundado el 5 de Agosto
           de 1968, por un grupo de vecinos comerciantes del departamento.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Next.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Sanity.io
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Vercel
-            </div>
-          </div>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/bomberos-voluntarios-de-lujan-de-cuyo"
+            webLink="https://bomberoslujandecuyo.org.ar/"
+          />
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Next.js" />
+            <TechsItem title="Sanity.io" />
+            <TechsItem title="Vercel" />
+          </Techs>
         </Step>
-      </ul>
-      <ul>
+      </BigStep>
+      <BigStep>
         <Step
           title="Centro de Estudios Ajmátova"
           link="https://www.centroestudiosajmatova.com.ar/"
         >
           La enseñanza de los matices lingüísticos y la gramática del Español
           inmersos en las culturas Latinoamericana y Rusa.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Gatsby.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Sanity.io
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Netlify
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Gatsby.js" />
+            <TechsItem title="Sanity.io" />
+            <TechsItem title="Netlify" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/centro-de-estudios-ajmatova"
+            webLink="https://www.centroestudiosajmatova.com.ar/"
+          />
         </Step>
-      </ul>
+      </BigStep>
       <Divider />
       <Year>2020</Year>
-      <ul>
+      <BigStep>
         <Step title="Sol Etchegaray" link="https://www.soletchegaray.com.ar/">
           Web de fotografía donde se busca naturalizar el cuerpo desnudo y
           visibilizar la diversidad de los cuerpos, captando los detalles tan
-          bellos y únicos que nos caracterizan
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Gatsby.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Contentful
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Netlify
-            </div>
-          </div>
+          bellos y únicos que nos caracterizan.
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Gatsby.js" />
+            <TechsItem title="Contentful" />
+            <TechsItem title="Netlify" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/sol-etchegaray/"
+            webLink="https://www.soletchegaray.com.ar/"
+          />
         </Step>
         <Step title="Cucha Estudio" link="https://www.cuchaestudio.com/">
           Desarrollo del sitio web para Cucha, un estudio de diseño con base en
           Barcelona, España. especializado en diseños de marcas, editorial y
           redes sociales.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Gatsby.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Contentful
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Netlify
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Gatsby.js" />
+            <TechsItem title="Contentful" />
+            <TechsItem title="Netlify" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/cucha-estudio/"
+            webLink="https://www.cuchaestudio.com/"
+          />
         </Step>
         <Step
           title="Germinación del Camino"
@@ -165,20 +186,16 @@ export default function Timeline() {
           App para facilitar el acceso a la información y se busca enfatizar las
           virtudes de las plantas por sobre las dolencias a las que pueden ser
           de utilidad.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Tailwind
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Gatsby.js
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Algolia
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Netlify
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Tailwind" />
+            <TechsItem title="Gatsby.js" />
+            <TechsItem title="Algolia" />
+            <TechsItem title="Netlify" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/germinacion-del-camino/"
+            webLink="https://germinacion.netlify.app/"
+          />
         </Step>
         <Step
           title="Alan Sutton"
@@ -186,19 +203,23 @@ export default function Timeline() {
         >
           Sitio web realizado adaptando la imagen y el diseño utilizado para el
           lanzamiento del disco La Era del Rivotril.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Wordpress
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Wordpress" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/alan-sutton"
+            webLink="https://www.alansutton.com.ar/biografia/"
+          />
         </Step>
         <Step title="Estudio Luis Romero" link="https://estudioluisromero.com/">
           Sitio web para el director de teatro Luis Romero
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Wordpress
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Wordpress" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/estudio-luis-romero"
+            webLink="https://estudioluisromero.com/"
+          />
         </Step>
         <Step
           title="Afda - Libre de VIolencias"
@@ -206,27 +227,31 @@ export default function Timeline() {
         >
           Sitio web para la Asociación Familias Diversas de Argentina sobre
           violencias y derechos sexuales.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Vue.js
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Vue.js" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/afda-libre-de-violencias"
+            webLink="https://www.afda.org.ar/libre-de-violencias/"
+          />
         </Step>
-      </ul>
+      </BigStep>
       <Divider />
       <Year>2019</Year>
-      <ul>
+      <BigStep>
         <Step title="Madejandola" link="http://madejandola.com/">
           Un universo repleto de historias que nacen en los hilos de América
           Latina.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Wordpress
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Wordpress" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/madejandola/"
+            webLink="http://madejandola.com/"
+          />
         </Step>
         <Step title="Fede Schrager" link="http://fedeschrager.com.ar/">
-          Maquetado de sitio web basado en el diseño de{' '}
+          Maquetado de sitio web basado en el gran diseño de{' '}
           <a
             className="underline"
             target="_blank"
@@ -235,26 +260,27 @@ export default function Timeline() {
           >
             mfetchegaray
           </a>
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              HTML & CSS
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="HTML & CSS" />
+          </Techs>
+          <ProjectsLinks webLink="http://fedeschrager.com.ar/" />
         </Step>
-      </ul>
+      </BigStep>
 
       <Divider />
       <Year>2018</Year>
-      <ul>
+      <BigStep>
         <Step title="Jibsa" link="http://jibsa.com.ar/">
           Una empresa comprometida con la preservación del patrimonio
           arquitectónico, la seguridad edilicia y las buenas formas
           empresariales.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Wordpress
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Wordpress" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/jibsa"
+            webLink="http://jibsa.com.ar/"
+          />
         </Step>
         <Step
           title="Clinicas Colectivas"
@@ -263,30 +289,28 @@ export default function Timeline() {
           Las Clínicas Colectivas de Artes Visuales es un proyecto que comenzó
           en el año 2013 con el objetivo de reunir y convocar artistas de la
           zona de la Comarca Andina del Paralelo 42.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Jekyll
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Github
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Jekyll" />
+            <TechsItem title="Github Pages" />
+          </Techs>
+          <ProjectsLinks
+            portfolioLink="https://www.santuan.com.ar/colaboraciones/clinicas-colectivas"
+            webLink="https://clinicascolectivas.github.io/"
+          />
         </Step>
-      </ul>
+      </BigStep>
 
       <Divider />
       <Year>2017</Year>
-      <ul>
+      <BigStep>
         <Step
           title="Saraña Huwitaqui"
           link="https://www.instagram.com/saranahuwitaqui/"
         >
           Un proyecto de Recreación, “Caminando Nuestra Bonita Tierra”.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Logo
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Logo" />
+          </Techs>
         </Step>
         <Step
           title="Vecinal del Paraje Entre Rios"
@@ -294,16 +318,13 @@ export default function Timeline() {
         >
           Vecinos y vecinas que nos juntamos a participar de esto llamado
           convivir.
-          <div className="mt-3">
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Jekyll
-            </div>
-            <div className="inline-block pr-2 mr-2 font-sans text-base font-bold text-gray-900 dark:text-white">
-              Github
-            </div>
-          </div>
+          <Techs>
+            <TechsItem title="Jekyll" />
+            <TechsItem title="Github Pages" />
+          </Techs>
+          <ProjectsLinks webLink="https://clinicascolectivas.github.io/" />
         </Step>
-      </ul>
+      </BigStep>
       {isShowingFullTimeline ? (
         <FullTimeline />
       ) : (
