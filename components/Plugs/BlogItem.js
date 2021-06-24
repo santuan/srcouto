81c2fce5401e6bb94cb186f09a038a52e3e98f2e
santuan <smrcouto88@gmail.com>
2021-06-23 21:30:12 -0300

    audio icon

--- components/Plugs/BlogItem.js
+++ components/Plugs/BlogItem.js
@@ -1,13 +1,15 @@
 import styles from './BlogPost.module.css';
 import Link from 'next/link';
+import { GiSpeaker } from "react-icons/gi";
 
-const BlogPost = ({ title, summary, slug }) => {
+const BlogPost = ({ title, summary, slug, audio }) => {
   return (
     <div className={styles.Container}>
       <Link href={`/blog/${slug}`}>
         <a className={styles.Title}>{title}</a>
       </Link>
       <p className={styles.Description}>{summary}</p>
+      {audio && <GiSpeaker className={styles.Icon}/>}
     </div>
   );
 };
