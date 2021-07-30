c05cca9ce5fe258bfc87c8510a6a748eaecc8db2
stn <smrcouto88@gmail.com>
2021-07-30 15:58:50 -0300

    footer

--- components/Bio/Trinomia.js
+++ components/Bio/Trinomia.js
@@ -33,7 +33,7 @@ const config = {
 export default function Trinomia() {
   const [isCopied, setIsCopied] = React.useState(false);
   return (
-    <div className="flex flex-col items-center justify-center text-sm text-blue-600 duration-300 grayscale hover:grayscale-0 hover:translate-y-2 hover:rotate-2 group">
+    <div className="flex flex-col items-center justify-center text-sm text-blue-600 duration-300 hover:translate-y-2 hover:rotate-2 group">
       <button
         onClick={() => {
           copyToClipboard('https://www.santuan.com.ar/');
@@ -41,26 +41,25 @@ export default function Trinomia() {
           setTimeout(() => setIsCopied(false), 3000);
         }}
         className={
-          'relative flex justify-center items-center flex-col mt-1 mx-2 rounded-md pb-1 font-bold font-sans dark:text-white text-gray-800  transform duration-700 ' +
-          (isCopied
-            ? '' + isCopied
-            : '')
+          'relative flex justify-center hover:bg-blue-100 dark:hover:bg-black px-4 py-2 hover:shadow-lg items-center border-0 flex-col mt-1 mx-2 rounded-md pb-1 font-bold font-sans dark:text-white text-gray-800  transform duration-700 ' +
+          (isCopied ? '' + isCopied : '')
         }
       >
-        
         <div className="flex space-x-1 ">
           <BsFillCircleFill className="text-blue-500" />
           <BsSquareFill className="text-red-500" />
           <BsTriangleFill className="text-yellow-500" />
         </div>
-        <span className="mt-2">{isCopied ? '🎉 Link copiado' : 'Web a color'}</span>
-        <span className="mb-2 font-sans text-xs font-bold text-gray-800 duration-300 md:opacity-0 dark:text-white group-hover:opacity-100">
-          santuan.com.ar
+        <span className="mt-2">
+          {isCopied ? 'Link copiado 🎉' : 'Link para copiar 📎'}
         </span>
-        <div className="transform -translate-y-12">
-          <Confetti active={isCopied} config={config} />
-        </div>
       </button>
+      <a href="https://www.santuan.com.ar/" target="_blank" rel="noopener noreferrer" className="my-2 font-sans text-xs font-bold text-gray-800 duration-300 md:opacity-0 dark:text-white hover:text-gray-500 group-hover:opacity-80">
+        Ir a una versión a color <br /> www.santuan.com.ar
+      </a>
+      <div className="transform -translate-y-12">
+        <Confetti active={isCopied} config={config} />
+      </div>
     </div>
   );
 }
